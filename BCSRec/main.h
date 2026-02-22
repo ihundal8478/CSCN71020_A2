9c8fd6a14a87bad3b73e60dcf8cced30c280279a
ihundal8478 <ihundal8478@conestogac.on.ca>
2026-02-21 19:04:57 -0500

    Base code corrected as per step 1

--- BCSRec/main.h
+++ BCSRec/main.h
@@ -1,8 +1,15 @@
 #pragma once
-void setLength(int input, int *length);
-void setWidth(int input, int *width);
-int getPerimeter(int *length, int *width);
-int getArea(int *length, int *width);
+#if defined(__cplusplus)
+extern "C" {
+#endif
+
+	void setLength(int input, int* length);
+	void setWidth(int input, int* width);
+	int getPerimeter(int* length, int* width);
+	int getArea(int* length, int* width);
 void printWelcomeMenu();
 void printOptions();
 int getIntInput(char message[]);
+#if defined(__cplusplus)
+}
+#endif
